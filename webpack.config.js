68f6ec4a7d38de6fcfc347f5f5edfca554b2ba67
trpowerr <mixazbubl@gmail.com>
2019-10-11 00:31:16 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+    module: {
+        rules: [
+            // {
+            //     test: /\.css$/,
+            //         use: [
+            //         MiniCssExtractPlugin.loader, 'css-loader',
+            //         ],
+            // }, 
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        // new MiniCssExtractPlugin({
+        //     filename: '[name].css',
+        // }),
+    ]
+};
